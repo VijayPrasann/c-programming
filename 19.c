f9f908a30824e257d1200afa1472cf3cc3e189be
VijayPrasann <123434049+VijayPrasann@users.noreply.github.com>
2023-01-27 15:54:58 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,13 @@
+/* C programing for adding two numbers*/ 
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value \n");
+	scanf("%d",&a);
+	printf("enter b value \n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("the sum of two numbers is %d",c);
+	return 0;
+}
