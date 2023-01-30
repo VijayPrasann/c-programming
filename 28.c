477ca33e0f5ac5699a2cb6ea6521f160d53b45b9
VijayPrasann <123434049+VijayPrasann@users.noreply.github.com>
2023-01-29 20:38:17 -0800

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,22 @@
+/* c program for calculating length of the string*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    char *ptr;
+
+    printf("Input a string : ");
+    gets(str);
+
+    ptr = str;
+    while (*ptr != '\0') {
+        length++;
+        ptr++;
+    }
+
+    printf("The length of the given string '%s' is : %d\n", str, length);
+
+    return 0;
+}
