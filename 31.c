b68e3f9ca2de79493f932779a88462108a1b2914
VijayPrasann <123434049+VijayPrasann@users.noreply.github.com>
2023-01-29 20:52:20 -0800

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,19 @@
+/* C program for finding factorial*/
+#include <stdio.h>
+
+void factorial(int *n, int *result) {
+    int i;
+    *result = 1;
+    for (i = 1; i <= *n; i++) {
+        *result *= i;
+    }
+}
+
+int main() {
+    int num, fact;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    factorial(&num, &fact);
+    printf("Factorial of %d is %d\n", num, fact);
+    return 0;
+}
